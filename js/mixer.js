929e34266ed37c7d3ab89377686f8b7594bf0e10
barbrakr <50030195+barbrakr@users.noreply.github.com>
2026-08-03 23:02:29 +0200

    Create mixer.js

---
+++ js/mixer.js
@@ -0,0 +1,73 @@
+export function mixBuffers(
+    main,
+    noise,
+    noiseLevel = 0.2
+){
+
+    const length =
+        main.length;
+
+
+    const result =
+        new AudioBuffer({
+            length,
+            numberOfChannels: 2,
+            sampleRate: main.sampleRate
+        });
+
+
+
+    for(
+        let ch = 0;
+        ch < 2;
+        ch++
+    ){
+
+        const out =
+            result.getChannelData(ch);
+
+
+        const source =
+            main.getChannelData(
+                Math.min(
+                    ch,
+                    main.numberOfChannels-1
+                )
+            );
+
+
+        const noiseData =
+            noise.getChannelData(ch);
+
+
+
+        for(
+            let i = 0;
+            i < length;
+            i++
+        ){
+
+            out[i] =
+                source[i]
+                +
+                noiseData[i]
+                *
+                noiseLevel;
+
+
+            // prevent clipping
+
+            if(out[i] > 1)
+                out[i]=1;
+
+            if(out[i] < -1)
+                out[i]=-1;
+
+        }
+
+    }
+
+
+    return result;
+
+}
