87ec7580d46b2bdee9027b96f32ac23814a18962
barbrakr <50030195+barbrakr@users.noreply.github.com>
2026-08-03 18:02:35 +0200

    Update processor.js

--- js/processor.js
+++ js/processor.js
@@ -5,185 +5,60 @@ import {
 } from "./audiogram.js";
 
 
-export async function applyHearingLoss(audioBuffer, audioContext){
+export async function applyHearingLoss(audioBuffer){
 
-    const sampleRate = audioBuffer.sampleRate;
-    const length = audioBuffer.length;
-
-    const offline =
+    const context =
         new OfflineAudioContext(
-            2,
-            length,
-            sampleRate
+            audioBuffer.numberOfChannels,
+            audioBuffer.length,
+            audioBuffer.sampleRate
         );
 
 
-    const outputBuffer =
-        offline.createBuffer(
-            2,
-            length,
-            sampleRate
-        );
+    const source =
+        context.createBufferSource();
+
+    source.buffer = audioBuffer;
 
 
-    const left =
-        audioBuffer.getChannelData(0);
+    let node = source;
 
 
-    const right =
-        audioBuffer.numberOfChannels > 1
-        ? audioBuffer.getChannelData(1)
-        : left;
+    const loss =
+        getLeftLoss();
 
 
-    const leftProcessed =
-        processChannel(
-            left,
-            sampleRate,
-            getLeftLoss()
-        );
+    for(let i=0;i<frequencies.length;i++){
+
+        const filter =
+            context.createBiquadFilter();
+
+        filter.type = "peaking";
+
+        filter.frequency.value =
+            frequencies[i];
+
+        filter.Q.value =
+            1;
+
+        filter.gain.value =
+            -loss[i] * 0.15;
 
 
-    const rightProcessed =
-        processChannel(
-            right,
-            sampleRate,
-            getRightLoss()
-        );
+        node.connect(filter);
 
-
-    outputBuffer
-        .copyToChannel(
-            leftProcessed,
-            0
-        );
-
-
-    outputBuffer
-        .copyToChannel(
-            rightProcessed,
-            1
-        );
-
-
-    return outputBuffer;
-
-}
-
-
-
-function processChannel(
-    audio,
-    sampleRate,
-    loss
-){
-
-
-    const n = audio.length;
-
-
-    let real =
-        new Float32Array(n);
-
-    let imag =
-        new Float32Array(n);
-
-
-
-    // forward FFT
-
-    fft(
-        audio,
-        real,
-        imag
-    );
-
-
-
-    for(
-        let i=0;
-        i<n/2;
-        i++
-    ){
-
-        const hz =
-            i * sampleRate / n;
-
-
-        const attenuation =
-            interpolateLoss(
-                hz,
-                loss
-            );
-
-
-        const gain =
-            Math.pow(
-                10,
-                -attenuation / 20
-            );
-
-
-        real[i] *= gain;
-        imag[i] *= gain;
-
-        real[n-i-1] *= gain;
-        imag[n-i-1] *= gain;
-
+        node = filter;
     }
 
 
-
-    const output =
-        new Float32Array(n);
-
-
-    ifft(
-        real,
-        imag,
-        output
+    node.connect(
+        context.destination
     );
 
 
-    return output;
-
-}
+    source.start();
 
 
-
-function interpolateLoss(freq,loss){
-
-    if(freq < frequencies[0])
-        return loss[0];
-
-
-    for(
-        let i=0;
-        i<frequencies.length-1;
-        i++
-    ){
-
-        if(
-            freq >= frequencies[i] &&
-            freq <= frequencies[i+1]
-        ){
-
-            const t =
-            (freq-frequencies[i]) /
-            (frequencies[i+1]-frequencies[i]);
-
-
-            return (
-                loss[i] +
-                t *
-                (loss[i+1]-loss[i])
-            );
-
-        }
-
-    }
-
-
-    return loss[loss.length-1];
+    return await context.startRendering();
 
 }
